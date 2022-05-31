683d3df27c9a109f6cde6146da4ff333cea52e13
Çağdaş <cagdasbalkay@hotmail.com>
2022-05-31 13:36:54 +0300

    film rating project
    Film rating project by Cagdas Balkay 20703027

---
+++ rating_sys/Guncelle.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace film_rating
+{
+    /// <summary>
+    /// Interaction logic for Guncelle.xaml
+    /// </summary>
+    public partial class Guncelle : Window
+    {
+        public Guncelle()
+        {
+            InitializeComponent();
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { ID = 1, Movie = "John Doe",Genre = "Horror",ReleaseDate = new DateTime(1971, 7, 23) ,Rate = 5 });
+            employees.Add(new Employee() { ID = 2, Movie = "Jane Doe", Genre = "Comedy", ReleaseDate = new DateTime(1974, 1, 17) ,Rate =3 });
+            employees.Add(new Employee() { ID = 3, Movie = "Sammy Doe", Genre = "Sci-Fi", ReleaseDate = new DateTime(1991, 9, 2) ,Rate = 1 });
+
+            dgEmployee.ItemsSource = employees;
+        }
+        public class Employee
+        {
+            public int ID { get; set; }
+
+            public string Movie { get; set; }
+
+            public string Genre { get; set; }
+
+            public DateTime ReleaseDate { get; set; }
+
+            public int  Rate { get; set; }
+        }
+    }
+}
---
+++ rating_sys/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace film_rating
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            lbxMovie.Items.Add(tbxName.Text + "        " + cbxGenre.Text + "        " + datePickerMovie.Text + "        " + cbxRating.Text);
+            cmbx();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lstbx();
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+            lbxMovie.Items.RemoveAt
+
+       (lbxMovie.Items.IndexOf(lbxMovie.SelectedItem));
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Guncelle git = new Guncelle();
+            git.ShowDialog();
+        }
+
+        public void cmbx()
+        {
+            cbxRating.ClearValue(ComboBox.SelectedItemProperty);
+            cbxGenre.ClearValue(ComboBox.SelectedItemProperty);
+            tbxName.Clear();
+            datePickerMovie.ClearValue(DatePicker.SelectedDateProperty);
+        }
+
+        public void lstbx()
+        {
+            lbxMovie.Items.Clear();
+        }
+    }
+}
